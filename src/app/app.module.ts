d609d2b8fd99c55a1306d5d0ee491c61b7145651
sirSlappz <tamestranger43@gmail.com>
2022-09-06 14:01:24 -0500

    getting rid of header component and merging in home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialsModule } from './materials/materials.module';
-import { ContactDialog, HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     HomeComponent,
     ProjectsComponent,
-    ContactDialog
   ],
   imports: [
     BrowserModule,
